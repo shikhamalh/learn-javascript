cebb0b3230974084a4b285900b1d3540e4ef50ab
shikhamalh <147791704+shikhamalh@users.noreply.github.com>
2023-10-13 05:53:26 +0000

    new code added

--- LearnJS/MyFirstJS.js
+++ LearnJS/MyFirstJS.js
@@ -1,1 +1,22 @@
 console.log("Shikha")
+
+const myFullNameAsPerOffice = "Shikha Malhotra"
+
+var accName ="Shikha"
+let email = "shikha@xyz.com";
+
+accName = "ShikhaMalhotra"
+
+var isLoggedIn = false;
+
+console.log(isLoggedIn)
+console.log("isLoggedIn")
+const accNumber= "001a"
+var paymentMethod;
+console.log(paymentMethod)
+
+paymentMethod= "upi"
+console.log(paymentMethod)
+
+let accountState;
+console.table([isLoggedIn,accNumber,paymentMethod])
