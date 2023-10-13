0334eeba90df225c4dc339f3bfff25b6979900cd
shikhamalh <147791704+shikhamalh@users.noreply.github.com>
2023-10-13 06:02:22 +0000

    added new code with typeof

--- LearnJS/MyFirstJS.js
+++ LearnJS/MyFirstJS.js
@@ -20,3 +20,48 @@ console.log(paymentMethod)
 
 let accountState;
 console.table([isLoggedIn,accNumber,paymentMethod])
+
+
+let score = 20
+
+console.log(typeof score);
+console.log(typeof (score));
+ 
+let valueIn = Number(score)
+
+console.log(typeof valueIn);
+
+console.log(valueIn);
+
+console.log ("---------checking string---------------")
+score = "2a"
+console.log (typeof score)
+console.log(typeof (score));
+valueIn = Number(score)
+console.log(typeof valueIn);
+console.log(valueIn);
+
+console.log ("---------checking undefined---------------")
+
+score = undefined
+console.log(typeof score);
+console.log(typeof (score));
+ valueIn = Number(score)
+console.log(typeof valueIn);
+console.log(valueIn);
+
+console.log ("----------checking boolean--------------")
+score= false
+console.log(typeof score);
+console.log(typeof (score));
+ valueIn = Number(score)
+console.log(typeof valueIn);
+console.log(valueIn);
+
+console.log ("----------changing numbers to boolean--------------")
+score= 1
+console.log(typeof score);
+console.log(typeof (score));
+ valueIn = Boolean(score)
+console.log(typeof valueIn);
+console.log(valueIn);
