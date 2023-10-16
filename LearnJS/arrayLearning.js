f3790fc6637f4c850cfc0f529bdd385820657ff6
shikhamalh <147791704+shikhamalh@users.noreply.github.com>
2023-10-16 06:31:23 +0000

    commitlatest

--- LearnJS/arrayLearning.js
+++ LearnJS/arrayLearning.js
@@ -22,10 +22,12 @@ console.log("Combining 2 arrays in 1")
 let a1 = ['a','b','c']
 let a2 = ['j','k','l']
 //a1.push(a2) //not a correct practice to combine arrays
+console.log("=====By using concat func======")
 let a3 = a1.concat(a2) //firs way
 console.log(a3)
 
 yourArray=['a', 'b', 'c']
 yourArray2=['j','k','l']
+console.log("=====By using spread operator======")
 const spreadOutArray = [...yourArray, ...yourArray2] //second way
 console.log(spreadOutArray);
